60ed26a5c727ffbed83b9ba6edaa0ba501117460
oyunojino <dusrn2xlayjoh@gmail.com>
2025-06-30 13:33:38 +0900

    .

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,5 +1,21 @@
+"use client";
+
 import MapApiPage from "@/page/map/MapApiPage";
+import styled from "@emotion/styled";
+
+const Title = styled.h2`
+  font-size: 22px;
+  font-weight: bold;
+  margin-bottom: 24px;
+`;
 
 export default function MapPage() {
-  return <MapApiPage />;
+  return (
+    <main>
+      <section>
+        <Title>오시는 길</Title>
+        <MapApiPage />
+      </section>
+    </main>
+  );
 }
