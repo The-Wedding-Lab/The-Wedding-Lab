5aa10086f8331f3a0ff80902bc92aaed7f40051d
TKman <102001532+greekr4@users.noreply.github.com>
2025-07-24 12:11:17 +0900

    design: final
    - 파비콘
    - 버튼 통일화

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -3,22 +3,22 @@ import { NextRequest, NextResponse } from "next/server";
 export const GET = async (req: NextRequest) => {
   const prisma = new PrismaClient();
 
-  //   const posts = await prisma.community.findMany({
-  //     skip: 0,
-  //     take: 10,
-  //     orderBy: {
-  //       created_at: "desc", // 최신순 정렬
-  //     },
-  //     select: {
-  //       id: true,
-  //       user_id: true,
-  //       title: true,
-  //       wedding_data: true,
-  //       likes: true,
-  //       liked: true,
-  //       created_at: true,
-  //     },
-  //   });
+    const posts = await prisma.community.findMany({
+      skip: 0,
+      take: 10,
+      orderBy: {
+        created_at: "desc", // 최신순 정렬
+      },
+      select: {
+        id: true,
+        user_id: true,
+        title: true,
+        wedding_data: true,
+        likes: true,
+        liked: true,
+        created_at: true,
+      },
+    });
 
   const raw_query = await prisma.$queryRaw`
   SELECT wedding_id FROM community
